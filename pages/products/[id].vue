0cb35caec4beebaef05277d182d001579fad73e8
nikhilesh500 <nikhil.500apps@gmail.com>
2024-02-27 18:43:19 +0530

    First commit

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Product Details for {{ id }}</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi rem officiis eius inventore explicabo id, et similique natus dolores doloremque soluta odio adipisci enim unde sint recusandae ut. Porro, harum?</p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Products</p>
+  </div>
+</template>
+
+<script setup>
+
+  
+</script>
+
+<style lang="scss" scoped>
+
+</style>
